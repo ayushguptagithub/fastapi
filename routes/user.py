059a4c2f40f9e6da66e0461602a0c4fc5bc1642f
ayush <gayush2041@gmail.com>
2022-07-06 01:57:48 +0530

    changed again

--- routes/user.py
+++ routes/user.py
@@ -1,11 +1,12 @@
 from os import name
-from fastapi import FastAPI
+from fastapi import APIRouter
 from config.db import conn
 from models.index import users
 from schemas.index import User
 
 
-user = FastAPI()
+user = APIRouter()
+
 
 @user.get('/')
 async def read_data():
